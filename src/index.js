dbee53345c5f58d9baf5b73079d5134a1573a53d
sergiorodas <sergiorodasbj@hotmail.com>
2021-08-08 20:55:45 -0300

    Modifica el puerto

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-const express=require("express"),app=express(),path=require("path");app.set("port",4e3),app.set("views",path.join(__dirname,"views")),app.engine("html",require("ejs").renderFile),app.set("view engine","ejs"),app.use(require("./routes/")),app.use(express.static(path.join(__dirname,"public"))),app.listen(app.get("port"),()=>{console.log("Server on port",app.get("port"))});
+const express = require("express"),
+  app = express(),
+  path = require("path");
+app.set("port", process.env.PORT || 3000),
+  app.set("views", path.join(__dirname, "views")),
+  app.engine("html", require("ejs").renderFile),
+  app.set("view engine", "ejs"),
+  app.use(require("./routes/")),
+  app.use(express.static(path.join(__dirname, "public"))),
+  app.listen(app.get("port"), () => {
+    console.log("Server on port", app.get("port"));
+  });
